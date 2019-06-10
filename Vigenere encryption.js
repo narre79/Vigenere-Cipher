1ae856c5fa4ee0d9133d115bfc7bd144ce549512
normdev <49796937+narre79@users.noreply.github.com>
2019-06-10 14:13:46 -0400

    Create Vigenere encryption.js
    Function takes plain text with a pass phrase and returns encrypted message

---
+++ Vigenere encryption.js
@@ -0,0 +1,9 @@
+//Vigenere encryption module
+
+'use strict'
+/* function takes plain text with a pass phrase and returns encrypted message */
+function (plainText, passPhrase) {
+
+
+  return console.log(encryptedText)
+}
